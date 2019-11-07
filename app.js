b8d29bf88094560bc49a0743182523cec040d595
Eloka Udoji <profeloka@yahoo.com>
2019-11-07 18:12:12 +0100

    local postgres setup

--- app.js
+++ app.js
@@ -1,9 +1,86 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const { pool } = require('./config');
 
 const app = express();
 
-app.use((req, res) => {
-    res.json({ message: 'Your request was successful!' });
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
 });
+
+app.use(bodyParser.json());
+
+const getBooks = (request, response) => {
+    pool.query('SELECT * FROM books', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const addBook = (request, response) => {
+    const { author, title } = request.body
+
+    pool.query('INSERT INTO books (author, title) VALUES ($1, $2)', [author, title], error => {
+        if (error) {
+            throw error
+        }
+        response.status(201).json({ status: 'success', message: 'Book added.' })
+    })
+}
+
+app
+    .route('/books')
+    // GET endpoint
+    .get(getBooks)
+    // POST endpoint
+    .post(addBook)
+
+app
+    .route('/api')
+    // GET endpoint
+    .get(getBooks)
+    // POST endpoint
+    .post(addBook)
+
+/*app.use((req, res) => {
+    res.json({ message: 'Your request was successful!' });
+});*/
+
+/*app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+        message: 'Thing created successfully!'
+    });
+});
+
+
+app.get('/api/stuff', (req, res, next) => {
+    const stuff = [
+        {
+            _id: 'oeihfzeoi',
+            title: 'My first thing',
+            description: 'All of the info about my first thing',
+            imageUrl: '',
+            price: 4900,
+            userId: 'qsomihvqios',
+        },
+        {
+            _id: 'oeihfzeomoihi',
+            title: 'My second thing',
+            description: 'All of the info about my second thing',
+            imageUrl: '',
+            price: 2900,
+            userId: 'qsomihvqios',
+        },
+    ];
+    res.status(200).json(stuff);
+});*/
+
+
 
 module.exports = app;
---
+++ init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    gender VARCHAR(255) NULL,
+    role_id VARCHAR(255) NOT NULL,
+    department VARCHAR(255) NULL,
+    address VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
